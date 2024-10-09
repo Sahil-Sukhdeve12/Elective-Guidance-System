79a4ca05487c0c55f8b37ea4379e357609523098
unknown <sahilsukhdeve15@gmail.com>
2024-10-09 22:20:29 +0530

    idk

--- Elective Guidance System/src/components/signup.jsx
+++ Elective Guidance System/src/components/signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { doCreateUserWithEmailAndPassword } from '../firebase/auth';
 import 'bootstrap/dist/css/bootstrap.min.css';
